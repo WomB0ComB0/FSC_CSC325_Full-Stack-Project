506606cf8355ca7a61c6dcbedadb96f3cbcf886c
Mike Odnis <airwolf635@gmail.com>
2024-06-24 22:20:18 -0400

    chore: initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.fsc_csc325_fullstackproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.fsc_csc325_fullstackproject to javafx.fxml;
+    exports org.example.fsc_csc325_fullstackproject;
+}
