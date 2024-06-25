0c7d337b5c9d1605b8029ba84308be4e5c69fc91
Mike Odnis <airwolf635@gmail.com>
2024-06-24 23:31:55 -0400

    chore: cross device 3

--- src/main/java/org/example/fsc_csc325_fullstackproject/MainApp.java
+++ src/main/java/org/example/fsc_csc325_fullstackproject/MainApp.java
@@ -2,7 +2,6 @@ package org.example.fsc_csc325_fullstackproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,7 +12,7 @@ import java.util.Objects;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader root = new FXMLLoader(MainApp.class.getResource("/view/PersonView.fxml"));
+        FXMLLoader root = new FXMLLoader(MainApp.class.getResource("/org/example/fsc_csc325_fullstackproject/view/PersonView.fxml"));
         Scene scene = new Scene(root.load());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/org/example/fsc_csc325_fullstackproject/view/styles.css")).toExternalForm());
         stage.setTitle("FSC CSC325 _ Full Stack Project");
